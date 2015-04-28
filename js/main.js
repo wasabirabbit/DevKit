5ee8a593764ae63b2077bcc6b9485ca4f26d1bfa
joeymilanes <jmilanes@wasabirabbit.com>
2015-04-28 16:29:56 -0400

    Haverhill

--- js/main.js
+++ js/main.js
@@ -10,4 +10,43 @@ $(document).ready(function () {
             initFlexSliderPlugin();
         }
     }
+
+ $('.flex-vertical').flexslider({
+    animation: "slide",
+    slideshow: true,
+    reverse: true,
+    animationLoop: true,   
+    touch: false,
+    keyboard: true 
+    // mousewheel: true
+    }); 
+});
+ 
+
+// Init SmoothScroll
+
+$('.smoothscroll').smoothScroll({offset: -95});
+
+
+$(function(){    
+   var urlHash = window.location.href.split("#")[1];
+    if (urlHash.length > 0)
+        $('html,body').animate({
+            scrollTop: $('#' + urlHash).offset().top -130
+        }, 500);    
+});
+
+
+// Menu
+$('.menu-toggle').on('click', function() {
+  $(this).toggleClass('open');
+});
+
+
+$('.menu-toggle').click(function () {
+    $('section.menu-slideout').toggleClass('show');
+});
+
+$('.menu-toggle.open').click(function () {
+    $('section.menu-slideout').removeClass('show');
 });
