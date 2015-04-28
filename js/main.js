d2171631ad9dd3f2ebffe72dfe46d8f3c6e3532e
joeymilanes <jmilanes@wasabirabbit.com>
2015-04-28 17:01:05 -0400

    Vertgid and JS fix

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
-    //Plugin check - flexslider
-    if ($(".flexslider").length > 0) {
-        if (!jQuery().flexslider) {
-            $.getScript("/Scripts/jquery.flexslider-min.js", function () {
-                initFlexSliderPlugin();
-            });
-        }
-        else {
-            initFlexSliderPlugin();
-        }
-    }
+
 
  $('.flex-vertical').flexslider({
     animation: "slide",
@@ -20,22 +10,8 @@ $(document).ready(function () {
     keyboard: true 
     // mousewheel: true
     }); 
-});
+
  
-
-// Init SmoothScroll
-
-$('.smoothscroll').smoothScroll({offset: -95});
-
-
-$(function(){    
-   var urlHash = window.location.href.split("#")[1];
-    if (urlHash.length > 0)
-        $('html,body').animate({
-            scrollTop: $('#' + urlHash).offset().top -130
-        }, 500);    
-});
-
 
 // Menu
 $('.menu-toggle').on('click', function() {
@@ -49,4 +25,19 @@ $('.menu-toggle').click(function () {
 
 $('.menu-toggle.open').click(function () {
     $('section.menu-slideout').removeClass('show');
+});
+
+// Init SmoothScroll
+
+    $('.smoothscroll').smoothScroll({offset: -95});
+
+
+$(function(){    
+   var urlHash = window.location.href.split("#")[1];
+    if (urlHash.length > 0)
+        $('html,body').animate({
+            scrollTop: $('#' + urlHash).offset().top -95
+        }, 2000);    
+});
+
 });
